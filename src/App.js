7332b1967244a7aff651673de9f8b114d1a87db5
unknown <yosefsitumorang97@gmail.com>
2021-04-12 08:41:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Preloader from "./components/Preloader/Preloader";
+// import Timer from "./components/Countdown/Timer";
+// import Optin from "./components/Optin/Optin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import "./App.css";
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="container">
+//         <h1>
+//           Website
+//           <br />
+//           Coming Soon
+//         </h1>
+//         <Timer />
+//         <Optin />
+//         <Preloader />
+//       </div>
+//     </div>
+//   );
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
