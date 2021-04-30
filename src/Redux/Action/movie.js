0ed7caf11f89206542b1da90a3a0a7f4d0fa2d34
unknown <yosefsitumorang97@gmail.com>
2021-04-30 16:51:15 +0700

    create action redux

---
+++ src/Redux/Action/movie.js
@@ -0,0 +1,74 @@
+import http from '../../../helper/http'
+
+export const allMovie = (token, search, order, limit, page, sort) => {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: 'SET_MOVIE_MESSAGE',
+        payload: ''
+      })
+      const results = await http(token).get(
+        `/movie?search=${search !== undefined ? search : ''}&limit=${
+          limit !== undefined ? limit : 4
+        }&page=${page !== undefined ? page : 1}&sort=${
+          sort !== undefined ? sort : 'id'
+        }&order=${order !== undefined ? order : 'ASC'}`
+      )
+      dispatch({
+        type: 'ALL_MOVIE',
+        payload: results.data.results
+      })
+      dispatch({
+        type: 'PAGE_INFO_ALL_MOVIE',
+        payload: results.data.pageInfo
+      })
+      dispatch({
+        type: 'IS_LOADING',
+        payload: false
+      })
+    } catch (err) {
+      console.log(err)
+      const { message } = err.response.data
+      dispatch({
+        type: 'SET_MOVIE_MESSAGE',
+        payload: message
+      })
+    }
+  }
+}
+
+export const detailMovie = (token, id) => {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: 'SHOWTIME_MESSAGE',
+        payload: ''
+      })
+      const results = await http(token).get(`/movie/${id}`)
+      dispatch({
+        type: 'DETAIL_MOVIE',
+        payload: results.data.results
+      })
+    } catch (err) {
+      console.log(err)
+      const { message } = err.response.data
+      dispatch({
+        type: 'SHOWTIME_MESSAGE',
+        payload: message
+      })
+    }
+  }
+}
+
+export const newDataMovieFlatList = (movie, pageInfo) => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'ALL_MOVIE',
+      payload: movie
+    })
+    dispatch({
+      type: 'PAGE_INFO_ALL_MOVIE',
+      payload: pageInfo
+    })
+  }
+}
---
+++ src/Redux/Reducer/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+import registerReducer from './register'
+import authReducer from './auth'
+import movieReducer from './movieDetail'
+
+const reducer = combineReducers({
+  auth: authReducer,
+  register: registerReducer,
+  movieDetail: movieReducer
+})
+
+export default reducer
