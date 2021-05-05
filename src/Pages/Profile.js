56355aea8175170c59b84bc670b56437251d8483
unknown <yosefsitumorang97@gmail.com>
2021-05-06 00:19:37 +0700

    create view all page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Navbar from '../components/Navbar'
+
+import Footer from '../components/Footer'
 
 export default class Profile extends Component {
   render () {
     return (
-      <div>
+      <>
+        <Navbar />
+
+        <Footer />
         Profile
-      </div>
+      </>
     )
   }
 }
