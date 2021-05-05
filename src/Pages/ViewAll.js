ce4becdd1453632bacd1ab44c2276179ec39afb3
unknown <yosefsitumorang97@gmail.com>
2021-05-06 00:20:38 +0700

    create view all page

--- src/Pages/ViewAll.js
+++ src/Pages/ViewAll.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Navbar from '../components/Navbar'
+
+import Footer from '../components/Footer'
 
 export default class ViewAll extends Component {
   render () {
     return (
-      <div>
+      <>
+        <Navbar />
+
+        <Footer />
         View All
-      </div>
+      </>
     )
   }
 }
