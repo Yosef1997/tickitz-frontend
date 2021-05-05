07632c680894ba28566b88e6a9ae4e89ef4c0f3e
unknown <yosefsitumorang97@gmail.com>
2021-05-06 00:14:22 +0700

    create seat order page

--- src/Pages/Seat.js
+++ src/Pages/Seat.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import Navbar from '../components/Navbar'
+
+import Footer from '../components/Footer'
+
 import { Link } from 'react-router-dom'
 
 export default class Seat extends Component {
   render () {
     return (
-      <div>
+      <>
+        <Navbar />
+
+        <Footer />
+
         <Link to="/movie/seat/payment">
-        seat order
+          seat order
         </Link>
-      </div>
+      </>
     )
   }
 }
