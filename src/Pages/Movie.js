77c9ef59686e8c00663f39b8a28f342529f98e02
unknown <yosefsitumorang97@gmail.com>
2021-05-05 14:02:27 +0700

    create dropdown

--- src/Pages/Movie.js
+++ src/Pages/Movie.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Detail from '../components/MovieDetail'
+import Dropdown from '../components/BtnDropdown'
 import Footer from '../components/Footer'
 
 export default class Movie extends Component {
@@ -10,6 +11,7 @@ export default class Movie extends Component {
       <>
         <Navbar />
         <Detail />
+        <Dropdown placeholder='Set a date' />
         <Footer />
         <Link to="/movie/seat">
         movie detail
