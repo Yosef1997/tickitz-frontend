d81a0454c5d58d51ff4ea10fba50406243159701
unknown <yosefsitumorang97@gmail.com>
2021-05-11 16:14:24 +0700

    integration end point show time

--- src/components/ShowTime/index.js
+++ src/components/ShowTime/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Select from 'react-select'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom'
 import moment from 'moment'
 import Calender from '../../assets/calender-icon.png'
@@ -20,7 +20,8 @@ class index extends React.Component {
     dataDate: [],
     dataLocation: [],
     date: '',
-    location: ''
+    location: '',
+    isLoading: false
   };
 
   async componentDidMount () {
@@ -47,12 +48,16 @@ class index extends React.Component {
     this.setState({ date })
   };
   handleLocation = async (location) => {
+    this.setState({ isLoading: true })
     const { token } = this.props.auth
     await this.props.detailLocation(token, location.value)
     this.setState({ location })
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 2000)
   };
   render () {
-    const { date, location, dataDate, dataLocation } = this.state
+    const { date, location, dataDate, dataLocation, isLoading } = this.state
 
     return (
       <Container fluid className='showTime'>
@@ -84,7 +89,16 @@ class index extends React.Component {
           </div>
         </Row>
         <Row>
-          <Cinema />
+          <Col className='showTimeCinema'>
+            {date === '' || location === ''
+              ? (<div className='showTimeCinemaText'>Please set a date and a location</div>)
+              : (
+                <>
+                {isLoading === true ? (<div className='d-flex justify-content-center mt-5'><Spinner animation="border" variant="dark" /></div>) : (<Cinema />)}
+                </>
+                )}
+
+          </Col>
         </Row>
       </Container>
     )
