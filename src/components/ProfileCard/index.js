b40b47390a2db6ba933abdeecab2c8d261fd8894
unknown <yosefsitumorang97@gmail.com>
2021-05-11 23:46:51 +0700

    integration end point profile

--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import './Profile.css'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { updateUser } from '../../Redux/Action/auth'
 import Profil from '../../assets/avanger.jpg'
 import Star from '../../assets/star.png'
 
-export default class index extends Component {
+class index extends Component {
   render () {
+    const { user } = this.props.auth
     return (
       <Container fluid className='profileCard'>
         <Row>
@@ -18,19 +21,23 @@ export default class index extends Component {
             <div onClick={() => console.log('image')} className='profileCardBtn'>
               <img src={Profil} className='profileCardImg' />
             </div>
-            <div className='profileCardName'>Jonas El Rodriguez</div>
+            {user.firstName === 'null' && user.lastName === 'null'
+              ? (<div className='profileCardName'>No Name</div>
+                )
+              : (<div className='profileCardName'>{`${user.firstName} ${user.lastName}`}</div>
+                )}
             <div className='profileCardText2'>Moviegoers</div>
             <div className='profileCardLoyalty'>
               <div className='profileCardText1 font-weight-bold'>Loyalty Points</div>
               <div className="rectangle1">
                 <div className="rectangle2 text-start ps-3">
-                    <div className="Moviegoers">Moviegoers</div>
-                    <p>320<span>points</span></p>
+                  <div className="Moviegoers">Moviegoers</div>
+                  <p>320<span>points</span></p>
                 </div>
                 <img src={Star} className="star" alt="star" />
-                  <div className="circle1 rounded-circle">
-                    <div className="circle2 rounded-circle"></div>
-                  </div>
+                <div className="circle1 rounded-circle">
+                  <div className="circle2 rounded-circle"></div>
+                </div>
               </div>
               <div className='profileCardText1 text-center mb-2'>{`${'180'} points become a master`}</div>
               <div className="progress">
@@ -43,3 +50,9 @@ export default class index extends Component {
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+const mapDispatchToProps = { updateUser }
+export default connect(mapStateToProps, mapDispatchToProps)(index)
