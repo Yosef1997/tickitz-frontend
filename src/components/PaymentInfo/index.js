6d95efefd53343fa6427d73c4aba1e4679aedb39
unknown <yosefsitumorang97@gmail.com>
2021-05-12 10:42:33 +0700

    integration end point payment

--- src/components/PaymentInfo/index.js
+++ src/components/PaymentInfo/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import { Link, withRouter } from 'react-router-dom'
+import { Formik } from 'formik'
+import { connect } from 'react-redux'
+import { updateUser } from '../../Redux/Action/auth'
 import './PaymentInfo.css'
 import BCA from '../../assets/bca.png'
 import BRI from '../../assets/bri.png'
@@ -15,95 +18,137 @@ import Input from '../Input'
 import InputNumber from '../InputNumber'
 
 class index extends Component {
-  handlePay =() => {
+  handlePay = () => {
     this.props.history.push('/movie/seat/payment/ticket')
   }
   render () {
     return (
       <Container fluid className='payment'>
-        <Row>
-          <Col lg={8}>
-            <div className='paymentTitle orderWeb'>Payment Info</div>
-            <div className='paymentForm'>
-              <div className='paymentDetail'>
-                <div className='paymentDetail1'>Date & time</div>
-                <div className='paymentDetail2'>{`${'Tuesday, 07 July 2020'} at ${'02:00pm'}`}</div>
-              </div>
-              <div className='paymentDetail'>
-                <div className='paymentDetail1'>Movie title</div>
-                <div className='paymentDetail2'>Spider-Man: Homecoming</div>
-              </div>
-              <div className='paymentDetail'>
-                <div className='paymentDetail1'>Cinema name</div>
-                <div className='paymentDetail2'>CineOne21 Cinema</div>
-              </div>
-              <div className='paymentDetail'>
-                <div className='paymentDetail1'>Number of tickets</div>
-                <div className='paymentDetail2'>3 pieces</div>
-              </div>
-              <div className='paymentDetail border-0'>
-                <div className='paymentDetail1'>Total payment</div>
-                <div className='paymentDetail2'>{`$${'30'},00`}</div>
-              </div>
-            </div>
+        <Formik
+          initialValues={{
+            fullName: '',
+            email: '',
+            phoneNumber: ''
+          }}
+          // validate={(values) => this.profileValidation(values)}
+          onSubmit={(values, { resetForm }) => {
+            this.setState({ isLoading: true })
+            this.doUpdate(values)
+            setTimeout(() => {
+              resetForm()
+            }, 500)
+          }}
+        >
+          {({ values, errors, handleChange, handleBlur, handleSubmit }) => (
+            <>
+              <Row>
+                <Col lg={8}>
+                  <div className='paymentTitle orderWeb'>Payment Info</div>
+                  <div className='paymentForm'>
+                    <div className='paymentDetail'>
+                      <div className='paymentDetail1'>Date & time</div>
+                      <div className='paymentDetail2'>{`${'Tuesday, 07 July 2020'} at ${'02:00pm'}`}</div>
+                    </div>
+                    <div className='paymentDetail'>
+                      <div className='paymentDetail1'>Movie title</div>
+                      <div className='paymentDetail2'>Spider-Man: Homecoming</div>
+                    </div>
+                    <div className='paymentDetail'>
+                      <div className='paymentDetail1'>Cinema name</div>
+                      <div className='paymentDetail2'>CineOne21 Cinema</div>
+                    </div>
+                    <div className='paymentDetail'>
+                      <div className='paymentDetail1'>Number of tickets</div>
+                      <div className='paymentDetail2'>3 pieces</div>
+                    </div>
+                    <div className='paymentDetail border-0'>
+                      <div className='paymentDetail1'>Total payment</div>
+                      <div className='paymentDetail2'>{`$${'30'},00`}</div>
+                    </div>
+                  </div>
 
-            <div className='paymentTitle orderWeb'>Choose a Payment Method</div>
-            <div className='paymentForm'>
-              <div className='paymentBtnForm'>
-                {[BCA, BRI, Dana, GooglePay].map((logo, idx) => {
-                  return (
-                    <>
-                      <Button className='paymentBtn' key={idx}>
-                        <img src={logo} fluid />
-                      </Button>
-                    </>
-                  )
-                })}
-              </div>
-              <div className='paymentBtnForm'>
-                {[GoPay, OVO, PayPal, Visa].map((logo, idx) => {
-                  return (
-                    <>
-                      <Button className='paymentBtn' key={idx}>
-                        <img src={logo} fluid />
-                      </Button>
-                    </>
-                  )
-                })}
-              </div>
-              <div className='paymentOr'>or</div>
-              <div className='paymentOr'>Pay via cash. <Link className='paymentCash'>See how it work</Link></div>
-            </div>
-
-            <div className='payBtnFrom'>
-              <Link to='/movie/seat' className='paymentBackStep'>Prvious step</Link>
-              <Button onClick={this.handlePay} className='paymentPay'>Pay your order</Button>
-            </div>
-
-          </Col>
-          <Col lg={4}>
-            <div className='paymentTitle orderWeb'>Personal Info</div>
-            <div className='PersonalInfoForm'>
-                <div className='mb-4'>
-                <Input label='Full Name' type='text' placeholder='Write your full name' />
-                </div>
-                <div className='mb-4'>
-                <Input label='Email' type='email' placeholder='Write your email' />
-                </div>
-                <div className='mb-4'>
-                <InputNumber label='Phone Number' type='text' placeholder='Write your phone number' />
-                </div>
-                <div className='PersonalInfoCaution'>
-                  <img src={Warning} className='mr-3' />
-                  <div>Fill your data correctly.</div>
-                </div>
-
-            </div>
-          </Col>
-        </Row>
+                  <div className='paymentTitle orderWeb'>Choose a Payment Method</div>
+                  <div className='paymentForm'>
+                    <div className='paymentBtnForm'>
+                      {[BCA, BRI, Dana, GooglePay].map((logo, idx) => {
+                        return (
+                          <>
+                            <Button className='paymentBtn' key={idx}>
+                              <img src={logo} fluid />
+                            </Button>
+                          </>
+                        )
+                      })}
+                    </div>
+                    <div className='paymentBtnForm'>
+                      {[GoPay, OVO, PayPal, Visa].map((logo, idx) => {
+                        return (
+                          <>
+                            <Button className='paymentBtn' key={idx}>
+                              <img src={logo} fluid />
+                            </Button>
+                          </>
+                        )
+                      })}
+                    </div>
+                    <div className='paymentOr'>or</div>
+                    <div className='paymentOr'>Pay via cash. <Link className='paymentCash'>See how it work</Link></div>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <div className='paymentTitle orderWeb'>Personal Info</div>
+                  <div className='PersonalInfoForm'>
+                    <div className='mb-4'>
+                      <Input
+                        label='Full Name'
+                        type='text'
+                        value={values.fullName}
+                        onChange={handleChange('fullName')}
+                        onBlur={handleBlur('fullName')}
+                        placeholder='Write your full name' />
+                    </div>
+                    <div className='mb-4'>
+                      <Input
+                        label='Email'
+                        type='email'
+                        value={values.email}
+                        onChange={handleChange('email')}
+                        onBlur={handleBlur('email')}
+                        placeholder='Write your email' />
+                    </div>
+                    <div className='mb-4'>
+                      <InputNumber
+                        label='Phone Number'
+                        type='text'
+                        value={values.phoneNumber}
+                        onChange={handleChange('phoneNumber')}
+                        onBlur={handleBlur('phoneNumber')}
+                        placeholder='Write your phone number' />
+                    </div>
+                    <div className='PersonalInfoCaution'>
+                      <img src={Warning} className='mr-3' />
+                      <div>Fill your data correctly.</div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col lg={8}>
+                  <div className='payBtnFrom'>
+                    <Link to='/movie/seat' className='paymentBackStep'>Prvious step</Link>
+                    <Button onClick={this.handlePay} className='paymentPay'>Pay your order</Button>
+                  </div>
+                </Col>
+              </Row>
+            </>
+          )}
+        </Formik>
       </Container>
     )
   }
 }
-
-export default withRouter(index)
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+const mapDispatchToProps = { updateUser }
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(index))
